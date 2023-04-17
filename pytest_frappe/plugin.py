fe1a6599fc3b987b966e2f688a5c901ead7050ec
saif <sirsaif99@gmail.com>
2023-04-17 05:28:20 +0200

    v0.0.6

--- pytest_frappe/plugin.py
+++
@@ -1,8 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption(
-        "--sites-path",
-        action="store",
-        dest="sites_path",
-        default="config",
-        help="Frappe config file",
-    )
